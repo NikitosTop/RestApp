8b2eb68dcd7a3eda02a211e7a9f065b7643cb4a3
Nikita Topchiy <blocksa59@gmail.com>
2021-11-18 22:49:27 +0200

    comments

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -7,6 +7,7 @@ class MapUtils {
     public static void mapShare(Map<String, String> map) {
         map.remove("c");
         if(map.containsKey("a")){map.put("b", map.get("a"));}
+        //this
     }
 
 }
