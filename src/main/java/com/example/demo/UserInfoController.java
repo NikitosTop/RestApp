98889773e9ac6a846dae436c53b304cdab6832bc
Nikita Topchiy <blocksa59@gmail.com>
2021-11-19 01:01:03 +0200

    delete post request

--- src/main/java/com/example/demo/UserInfoController.java
+++ src/main/java/com/example/demo/UserInfoController.java
@@ -5,11 +5,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class UserInfoController {
 
-    @PostMapping("/user")
-    public String userStatus(@RequestBody String user){
-        return user;
-    }
-
     @GetMapping("/getuser/{name}")
     public String getUserName(@PathVariable String name){
         return name;
